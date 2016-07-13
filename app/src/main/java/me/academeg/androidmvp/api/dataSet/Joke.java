00bce7b173ace8fa5833f9540ae14ee7baad8b9c
Yuriy Samsonov <belomore2007@gmail.com>
2016-07-13 19:58:36 +0300

    Change parser for Joke

--- app/src/main/java/me/academeg/androidmvp/api/dataSet/Joke.java
+++ app/src/main/java/me/academeg/androidmvp/api/dataSet/Joke.java
@@ -18,12 +18,14 @@ public class Joke implements Serializable {
 
     public static Joke parse(JSONObject obj) throws JSONException {
         Joke joke = new Joke();
-        joke.id = obj.getInt("id");
-        joke.joke = obj.getString("joke");
-        JSONArray categories = obj.getJSONArray("categories");
-        joke.categories = new String[categories.length()];
-        for (int i = 0; i < categories.length(); i++) {
-            joke.categories[i] = categories.getString(i);
+        joke.id = obj.optInt("id");
+        joke.joke = obj.optString("joke");
+        JSONArray categories = obj.optJSONArray("categories");
+        if (categories != null) {
+            joke.categories = new String[categories.length()];
+            for (int i = 0; i < categories.length(); i++) {
+                joke.categories[i] = categories.optString(i);
+            }
         }
         return joke;
     }
